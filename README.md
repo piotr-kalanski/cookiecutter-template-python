1fa09895253a10a1a7e97da1315947c39cd2a610
piotr-kalanski <piotr.kalanski@gmail.com>
2020-03-24 18:58:07 +0100

    first template version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cookiecutter-template-python
+
 Cookiecutter template for Python application
+
+## Create directory structure
+
+    cookiecutter https://github.com/piotr-kalanski/cookiecutter-template-python.git
---
+++ {{cookiecutter.main_directory}}/test/test_example.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class ExampleTest(unittest.TestCase):
+
+    def setUp(self):
+        ...
+        # TODO - implementation
+
+    # TODO - change method name
+    def test_xyz(self):
+        ...
+        # TODO - implementation
+        # self.assertEqual("d1", "d1")
